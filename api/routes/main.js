bfd11771a98b165291bdf728fc77658922effb54
Tomer Lev <tomer.l@lusha.com>
2021-06-09 21:58:08 +0300

    first commit

---
+++ api/routes/main.js
@@ -0,0 +1,29 @@
+const app = require('../app')
+const axios = require('axios')
+
+function loopResturant(resturant) {
+    console.log(`arg was => ${resturant}`);
+    const response = await axios.get(`https://restaurant-api.wolt.com/v3/venues/slug/${resturant}`)
+    const online = response.results[0].online
+    console.log(online);
+    if (online) {
+        const r = await axios.post("https://api.pushover.net/1/messages.json", data = {
+            "token": "ahckimerwqwhickwcku8a14qa1tfys",
+            "user": "uiefspi2oys2pgo2pfhh7h31dvpb6t",
+            "message": `Resturant ${resturant} is back online!!`
+        })
+        console.log(r.data);
+        return
+    }
+    setTimeout(loopResturant, 5000, resturant)
+
+  }
+  
+
+app.get('/api/wolt', async (req, res) => {
+    const rest = req.query.resturant
+    loopResturant(rest)
+    return res.status(200).send({ "resturant": rest, "message": "Watcher setted"})
+})
+
+module.exports = app
---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div  class="content countdown-timer">
+    <input type="text" placeholder="Paste URL Here!" v-model="resturant" />
+    <button @click="setRestu">Send To Watcher </button>
+    <div>
+      {{ json_obj }}
+    </div>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap');
+.content {
+  max-width: 900px;
+  margin: auto;
+  text-align: center;
+}
+:root {
+  --backgroundColor: rgba(246, 241, 209);
+  --colorShadeA: rgb(106, 163, 137);
+  --colorShadeB: rgb(121, 186, 156);
+  --colorShadeC: rgb(150, 232, 195);
+  --colorShadeD: rgb(187, 232, 211);
+  --colorShadeE: rgb(205, 255, 232);
+}
+* {
+  box-sizing: border-box;
+}
+*::before, *::after {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Amatic SC', 'OpenSans', sans-serif;
+  font-size: 3rem;
+  line-height: 2;
+  display: flex;
+          align-items: center;
+          justify-content: center;
+  margin: 0;
+  min-height: 100vh;
+  background: var(--backgroundColor);
+}
+
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        json_obj: {},
+        resturant: ""
+      }
+  },
+  fetch() {
+    
+  },
+  created() {
+    
+  },
+  methods: {
+    setRestu: function () {
+      this.$axios.get(`/api/wolt?resturant=${this.resturant}`).then(response => {
+          console.log(response);
+          this.json_obj = response.data
+      })
+    }
+  }
+
+  }
+</script>
